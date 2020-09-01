aa89e65c735a2fc974ad9a1bab6a9e5ba3d76c8e
13manaboy <dengsijian@gmail.com>
2020-09-02 00:14:32 +1000

    restrict homepage to empty url suffix

--- app/izcert/urls.py
+++ app/izcert/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 urlpatterns = [
     # Root page -> welcome page
-    path('', include('welcome.urls')),
+    re_path(r'^$', include('welcome.urls')),
 
     # Admin page
     path('admin/', admin.site.urls),
+
+    # Account auth
+    path('account/', include('django.contrib.auth.urls')),
 ]
