292c83ade093b90b03068278075c65366430649e
13manaboy <dengsijian@gmail.com>
2020-09-06 12:40:17 +1000

    restructured templates

--- app/izcert/settings.py
+++ app/izcert/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'auth/templates')
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
