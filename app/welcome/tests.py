d73ed01b5e4e70a2c7918bcd5ef2d793800c4254
13manaboy <dengsijian@gmail.com>
2020-09-07 23:23:02 +1000

    fixed tests on static files

--- app/welcome/tests.py
+++ app/welcome/tests.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.staticfiles.storage import staticfiles_storage #The file storage engine to use when collecting static files with the collectstatic management command.
 from django.contrib.staticfiles import finders
+from django.conf import settings
+import os
+
+BASE_DIR = settings.BASE_DIR
 
 # Create your tests here.
 class UrlTests(TestCase):
@@ -14,10 +18,15 @@ class StaticFileFinderTests(TestCase):
 
     # Doesn't work - need to come back
     def test_finder_static_files(self):
+        
+        static_dir = 'static'
+
         staticfiles = [
-            'css/bootstrap.min.css',
+            # 'css/bootstrap.min.css',
             'css/sticky-footer.css',
         ]
 
-        for fl in staticfiles:
-            self.assertTrue(staticfiles_storage.exists(finders.find(fl)))
+        for staticfile in staticfiles:
+            css_file_path = os.path.join(BASE_DIR, static_dir, staticfile)
+            result = finders.find(staticfile)
+            self.assertEqual(result, css_file_path)
