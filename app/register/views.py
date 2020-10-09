4c28a310fba59cac98c04c6b795dd846e2018dd7
choushishi <choushishi@users.noreply.github.com>
2020-10-09 20:00:04 +1100

    Auth (#19)
    * added sample data
    
    * added sample data
    
    * added register page
    
    * began fixing on frontend workflow
    
    * fixed register redirection error

--- app/register/views.py
+++ app/register/views.py
@@ -7,8 +7,7 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-
-        return redirect("/welcome")
+            return redirect("/welcome")
     else:
         form = RegisterForm()
 
